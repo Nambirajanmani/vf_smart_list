d6ed665b101d2a198949d5f10177fe39af72c510
nambirajanmani <nambirajan2802@mail.com>
2026-09-10 14:31:06 +0530

    Fix admin page routing and add _redirects for Render static site

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import AdminPage   from './pages/AdminPage.jsx';
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/"      element={<LandingPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
